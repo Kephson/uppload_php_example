fd4180231a3aa7209b1485e6c331587d247db202
Ephraim Härer <ephraim@ephespage.de>
2023-02-01 20:54:04 +0100

    splitted CSS into separate files, updated example files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -13,8 +14,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 };
